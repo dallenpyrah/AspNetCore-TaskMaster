feb993cf083e4542bb3723af9689e36994f643c9
Dallen Pyrah <dallenpyrah@gmail.com>
2021-05-22 20:49:04 -0600

    Practicing with another project

---
+++ Controllers/TaskController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Taskmaster.Data;
+
+namespace Taskmaster.Controllers
+{
+    public class TaskController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TaskController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Taskmaster.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public readonly DbContextOptions options;
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+            this.options = options;
+        }
+
+        public DbSet<Task> Tasks { get; set; }
+    }
+}
