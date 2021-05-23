50cbfce4baca31988c561c6663e53a7e53cf9e1d
Dallen Pyrah <dallenpyrah@gmail.com>
2021-05-22 20:37:06 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Taskmaster.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
